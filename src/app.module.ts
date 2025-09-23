49c3bb1b7e082c649b076131d86559d37bd951e0
Luis Prieto <luis.prieto@siesa.com>
2025-09-23 08:50:05 -0500

    changes for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public' ),
     }), 
-    MongooseModule.forRoot(process.env.MONGODB ?? ''),
+    MongooseModule.forRoot(process.env.MONGODB ?? '', {
+      dbName: 'pokemonsDB'
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
